c2da5c7b2a18530cd0ae8128967fa39213483e4d
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-08-23 11:58:45 -0400

    trying to push

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const numbers = Array.from(document.querySelectorAll(".number"));
 const twist = Array.from(document.querySelectorAll(".twist"));
+const hand = document.getElementById("minute-hand");
 
 for(let n in numbers){
     let x = 30 * n;
     numbers[n].style.transform = `rotate(`+x+`deg)`;
     twist[n].style.transform = `rotate(-`+x+`deg)`;
+}
+
+setInterval(turnSecond, 1000);
+
+let counter = 0;
+function turnSecond(){
+    let counterPlus = counter++;
+    let counterThird = counterPlus * 6;
+    hand.style.transform = `rotate(`+counterThird+`deg)`;
 }
