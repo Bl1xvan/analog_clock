c9082ac5f1224d544725bd1ac5cedec2bcb2e003
Bl1xvan <84408872+Bl1xvan@users.noreply.github.com>
2022-08-23 16:31:29 +0000

    redesigned the clock

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const numbers = Array.from(document.querySelectorAll(".number"));
 const twist = Array.from(document.querySelectorAll(".twist"));
-const hand = document.getElementById("minute-hand");
+const secondhand = document.getElementById("second-hand");
+const minutehand = document.getElementById("minute-hand")
 
 for(let n in numbers){
     let x = 30 * n;
@@ -9,10 +10,17 @@ for(let n in numbers){
 }
 
 setInterval(turnSecond, 1000);
+setInterval(turnMinute, 60000);
 
-let counter = 0;
+let counter = 1;
 function turnSecond(){
     let counterPlus = counter++;
     let counterThird = counterPlus * 6;
-    hand.style.transform = `rotate(`+counterThird+`deg)`;
+    secondhand.style.transform = `rotate(`+counterThird+`deg)`;
+}
+
+function turnMinute(){
+    let counterPlus = counter++;
+    let counterThird = counterPlus * 6;
+    minutehand.style.transform = `rotate(`+counterThird+`deg)`;
 }
