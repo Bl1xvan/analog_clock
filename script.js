21d9f5db5773a718633a622ed5faf2c0bbecb506
Bl1xvan <84408872+Bl1xvan@users.noreply.github.com>
2022-08-24 17:29:54 +0000

    Added 12 modulo format to hours

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const twist = Array.from(document.querySelectorAll(".twist"));
 const secondhand = document.getElementById("second-hand");
 const minutehand = document.getElementById("minute-hand")
 const hourhand = document.getElementById("hour-hand");
+const digitalclock = document.getElementById("digital-clock");
 
 for(let n in numbers){
     let x = 30 * n;
@@ -22,10 +23,12 @@ let startdate = new Date(new Date().toLocaleString("en-US", {
 let startseconds = startdate.getSeconds();
 let startminutes = startdate.getMinutes();
 let starthours = startdate.getHours();
+let starthourformat = starthours % 12 || 12;
 
 secondhand.style.transform = `rotate(${startseconds * 6}deg)`
 minutehand.style.transform = `rotate(${startminutes * 6}deg)`
 hourhand.style.transform = `rotate(${starthours* 30}deg)`
+digitalclock.innerText= `${starthourformat < 10 ? '0'+starthourformat : starthourformat}:${startminutes < 10 ?'0'+startminutes : startminutes}:${startseconds < 10 ? '0'+startseconds : startseconds}`
 
 console.log(starthours)
 
@@ -44,11 +47,14 @@ function turnSecond(){
     let seconds = date.getSeconds();
     let minutes = date.getMinutes();
     let hours = date.getHours();
+    let hourformat = hours % 12 || 12;
     let secondThird = seconds * 6;
     let minuteThird = minutes * 6;
     let hourThird = hours * 30;
     secondhand.style.transform = `rotate(`+secondThird+`deg)`;
     minutehand.style.transform = `rotate(`+minuteThird+`deg)`;
     hourhand.style.transform = `rotate(`+hourThird+`deg)`;
+
+  digitalclock.innerText= `${hourformat < 10 ? '0'+hourformat : hourformat}:${minutes < 10 ?'0'+minutes : minutes}:${seconds < 10 ? '0'+seconds : seconds}`
 }
 
