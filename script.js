41fd3af1742e3ad5f5ded96f91bc7b517427c8f5
Bl1xvan <84408872+Bl1xvan@users.noreply.github.com>
2022-08-24 13:03:11 +0000

    Added minute and hour hands

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const numbers = Array.from(document.querySelectorAll(".number"));
 const twist = Array.from(document.querySelectorAll(".twist"));
 const secondhand = document.getElementById("second-hand");
 const minutehand = document.getElementById("minute-hand")
+const hourhand = document.getElementById("hour-hand");
 
 for(let n in numbers){
     let x = 30 * n;
@@ -9,18 +10,45 @@ for(let n in numbers){
     twist[n].style.transform = `rotate(-`+x+`deg)`;
 }
 
+let startdate = new Date(new Date().toLocaleString("en-US", {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+}));
+let startseconds = startdate.getSeconds();
+let startminutes = startdate.getMinutes();
+let starthours = startdate.getHours();
+
+secondhand.style.transform = `rotate(${startseconds * 6}deg)`
+minutehand.style.transform = `rotate(${startminutes * 6}deg)`
+hourhand.style.transform = `rotate(${starthours* 30}deg)`
+
+console.log(starthours)
+
 setInterval(turnSecond, 1000);
-setInterval(turnMinute, 60000);
 
-let counter = 1;
 function turnSecond(){
-    let counterPlus = counter++;
-    let counterThird = counterPlus * 6;
-    secondhand.style.transform = `rotate(`+counterThird+`deg)`;
+  let date = new Date(new Date().toLocaleString("en-US", {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true
+}));
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let secondThird = seconds * 6;
+    let minuteThird = minutes * 6;
+    let hourThird = hours * 30;
+    secondhand.style.transform = `rotate(`+secondThird+`deg)`;
+    minutehand.style.transform = `rotate(`+minuteThird+`deg)`;
+    hourhand.style.transform = `rotate(`+hourThird+`deg)`;
 }
 
-function turnMinute(){
-    let counterPlus = counter++;
-    let counterThird = counterPlus * 6;
-    minutehand.style.transform = `rotate(`+counterThird+`deg)`;
-}
